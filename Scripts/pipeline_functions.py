d61ca45f5c04823f94fef2ed4526f6468c436e29
Maxime-Valeille <maxime.valeille@outlook.fr>
2026-02-13 16:32:06 +0000

    Correct codes

--- Scripts/pipeline_functions.py
+++ Scripts/pipeline_functions.py
@@ -1,12 +1,19 @@
 import os
+import sys
 import astropy.io.ascii
 import numpy as np
 import glob
 
+# Add the path to analysisUtils
+sys.path.append("/home/user/spxmv2/analysis_utilities/analysis_scripts/")
+import analysisUtils as au
+
+from casatools import msmetadata, table
+from casatasks import split, concat, tclean, imstat, imhead, uvcontsub, imcontsub, importfits, imreframe, imtrans, imregrid, immath, feather, immoments, exportfits
+
 
 ###fre_range in Hz[1.01e11, 1.05e11]
 def get_spw_number(fre_range, input_vis):
-    from casatools import msmetadata
     msmd = msmetadata()
     msmd.open(input_vis)
     # get the channel effective bandwidths for spectral window, in m/s or Hz (default)
@@ -25,7 +32,6 @@ def get_spw_number(fre_range, input_vis):
 
 ####C52_12.ms, get mean coordinates of all the fields
 def get_mean_phasecenter(input_vis):
-    from casatools import table
     tb = table()
     tb.open(input_vis + '/FIELD')
     ####RA, DEC IN RADIANS
@@ -48,7 +54,6 @@ def get_mean_phasecenter(input_vis):
 
 #####get the phasecenter coordinates of certain field
 def get_coor_phasecenter(input_vis, input_field):
-    from casatools import msmetadata
     msmd = msmetadata()
     msmd.open(input_vis)
     f_loc = msmd.phasecenter(input_field)
@@ -76,6 +81,13 @@ def get_linefree_chan(input_spw):
 ####select the science spw and target, then combine, if this hasn't been done
 
 def spw_target():
+    # Clean up any existing output files from previous runs
+    if os.path.exists('calibrated.ms'):
+        os.system('rm -rf calibrated.ms calibrated.ms.flagversions')
+    if os.path.exists('calibrated_final.ms'):
+        os.system('rm -rf calibrated_final.ms calibrated_final.ms.flagversions')
+    
+    msmd = msmetadata()
 
     vislist = glob.glob('*[!_ts].ms')
 
@@ -89,21 +101,30 @@ def spw_target():
         sciencespws = ','.join(map(str,sciencespws))
         msmd.close()
 
-        split(vis=myvis,outputvis=myvis+'.split.cal',spw=sciencespws)
+        output_vis = myvis+'.split.cal'
+        # Remove existing output file if it exists
+        if os.path.exists(output_vis):
+            os.system(f'rm -rf {output_vis}')
+        
+        split(vis=myvis,outputvis=output_vis,spw=sciencespws,datacolumn='all')
 
     vislist=glob.glob('*.ms.split.cal')
 
 
     concatvis='calibrated.ms'
-    #rmtables(concatvis)
-    #os.system('rm -rf ' + concatvis + '.flagversions')
+    # Remove existing output file if it exists
+    if os.path.exists(concatvis):
+        os.system(f'rm -rf {concatvis}')
+        os.system(f'rm -rf {concatvis}.flagversions')
     concat(vis=vislist, concatvis=concatvis)
 
     # in CASA, split only the sources data
     sourcevis='calibrated_final.ms'
-    #rmtables(sourcevis)
-    #os.system('rm -rf ' + sourcevis + '.flagversions')
-    split(vis=concatvis, intent='*TARGET*', outputvis=sourcevis, datacolumn='data')
+    # Remove existing output file if it exists
+    if os.path.exists(sourcevis):
+        os.system(f'rm -rf {sourcevis}')
+        os.system(f'rm -rf {sourcevis}.flagversions')
+    split(vis=concatvis, intent='*TARGET*', outputvis=sourcevis, datacolumn='data', observation='')
 
 
 ####iamge the continuum image for one ms.
--- Scripts/run_pipeline.py
+++ Scripts/run_pipeline.py
@@ -1,12 +1,19 @@
 import os
+import sys
 import astropy.io.ascii
 import numpy as np
 import glob
-import pipeline_functions
+
+# Add the Scripts directory to the path so pipeline_functions can be found
+script_dir = os.path.dirname(os.path.abspath(__file__))
+if script_dir not in sys.path:
+    sys.path.insert(0, script_dir)
+
+from pipeline_functions import *
 
 #"The code to run it"
 data = astropy.io.ascii.read('/home/user/spxmv2/LAGAF/clump_data_infor.csv')
-clump_id = np.array(data['source_id'])
+clump_id = np.array(data['clump_id'])
 loc_7m = np.array(data['7m_loc'])
 loc_12m = np.array(data['12m_loc'])
 loc_tp = np.array(data['tp_loc'])
@@ -23,7 +30,7 @@ clump_loc_folder = loc_folder[clump_loc]
 clump_subloc_tp = subloc_tp[clump_loc]
 
 basic_folder = '/export/home/spxnp1/LAGAF/2022.1.01204.S/'
-storage_folder = '/export/home/spxnp1/LAGAF/data_reduction/'
+storage_folder = '/home/reblodata/spxmv2/LAGAF/Data_reduction/'
 ####go to the folder and make a clump folder
 os.chdir(storage_folder)
 os.makedirs(clump_name+'_combine', exist_ok=True)
